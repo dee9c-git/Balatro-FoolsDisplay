d3e8dee2fdf66a651504f5454f983eaa43d608b3
Dee9c <aimccccccccc@gmail.com>
2026-09-25 20:55:00 +0800

    feat: shader version done

--- main.lua
+++ main.lua
@@ -4,16 +4,21 @@ SMODS.Atlas {
     py = G.ASSET_ATLAS['Tarot'].py,
     path = 'fools_display.png',
 }
-
+SMODS.Shader {
+    key = 'fools',
+    path = 'fools.fs',
+}
 SMODS.DrawStep {
     key = 'FoolsDisplay',
     order = 101,
     func = function(card, _)
         if card.ability.name == "The Fool" then
-            card.children.center.atlas = G.ASSET_ATLAS["FoolsDisplay_c_fool"]
             if G.GAME.last_tarot_planet ~= nil then
+                card.children.center.atlas = G.ASSET_ATLAS[G.P_CENTERS[G.GAME.last_tarot_planet].atlas]
                 card.children.center.sprite_pos = G.P_CENTERS[G.GAME.last_tarot_planet].pos
             end
+
+            card.children.center:draw_shader('FoolsDisplay_fools')
         end
     end,
     conditions = { vortex = false, facing = 'front' },
